2822870409f0ff0df14e520e064cbc7257ba3171
George Olufemi <georgeoluolufemi@gmail.com>
2022-07-21 22:34:50 +0100

    Modified && added content to Pages

--- README.md
+++ README.md
@@ -2,10 +2,20 @@
 
 This Website was built with [ React ](https://github.com/facebook/create-react-app).
 
-## Packages and Dependencies Installed
+## Libraries, Packages and Dependencies Installed
 
 ### `1.) TailwindCSS`
 ### `2.) React Router DOM`
 ### `3.) Axios`
 ### `4.) Material UI` 
 ### `3.) Flowbite`
+
+TailwindCSS was used as the main CSS framework used for the styling of the Website.
+
+React Router DOM was use for the routing between different pages on the website.
+
+Axios was use to fetch the data from the API endpoint given.
+
+Material UI was used for the dropdow component, this handled the styling and also the functionality of the dropdown.
+
+Flowbite is a Tailwind Plugin which allows for addition of functionalities to Tailwind's component library Utility classes. 
--- src/pages/UpcomingRides.jsx
+++ src/pages/UpcomingRides.jsx
@@ -1,11 +1,86 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
+import axios from "axios";
+import Spinner from '../components/Spinner';
+import Dropdown from '../components/Dropdown';
+import Skeleton from '../components/Skeleton';
 
 const UpcomingRides = () => {
+  const [rides, setRides] = useState();
+  useEffect(() => {
+    axios.get("https://assessment.api.vweb.app/rides")
+    .then(response => setRides(response.data))
+  }, []);
   return (
-    <div>
-      upcoming rides
-    </div>
+    <React.Fragment>
+      <div className="bg-[#292929] h-auto px-[43px] pt-[28px] pb-[100px]">
+        <div className="text-white flex justify-between mb-[24px]">
+          {/*  */}
+          <div className="flex items-center justify-between">
+          <Link to="/">
+            <div className="mr-[44px] cursor-pointer">
+              <h1 className="">Nearest rides</h1>
+            </div>
+          </Link>
+          <Link to="/upcoming-rides">
+            <div className="mr-[44px] cursor-pointer">
+              <h1 className="border-b-2">Upcoming rides</h1>
+            </div>
+          </Link>
+          <Link to="/past-rides">
+            <div className="mr-[44px] cursor-pointer">
+              <h1>Past rides</h1>
+            </div>
+          </Link>
+          </div>
+          {/*  */}
+          <div className="flex items-center justify-center cursor-pointer">
+            <div className="mr-[8px]">
+              <img src="/hamburger.png" alt="" />
+            </div>
+            <div>
+                <Dropdown />
+            </div>
+          </div>
+        </div>
+        {/* component */}
+        {
+          rides ? rides.map((ride, index) => (
+          <div key={index} className="bg-[#171717] w-[1350px] h-[198px] rounded-[10px] flex text-white mb-[13px]">
+            <div className="mt-[22px] ml-[29px] mr-[44px]">
+              <img className="rounded-[5px] w-[296px] h-[148px] object-fit" src={ride.map_url} alt="Map" />
+            </div>
+            <div className="mt-[25px] mr-[500px]">
+              <p className="line">Ride Id: {ride.id}</p>
+              <p className="line">Origin Station: {ride.origin_station_code}</p>
+              <p className="line">station_path: {ride.station_path}</p>
+              <p className="line">Date: {ride.date}</p>
+              <p className="line">Distance: 0</p>
+            </div>
+            <div className="mt-[26px] mr-[24px]">
+              <div className="w-[80px] h-[23px] bg-black rounded-[16px] text-white flex items-center justify-center">
+                <h1 className="font-[500] text-[12px] leading-[15px] ">{ride.city}</h1>
+              </div>
+            </div>
+            <div className="mt-[26px]">
+              <div className="w-[80px] h-[23px] bg-black rounded-[16px] text-white flex items-center justify-center">
+                <h1 className="font-[500] text-[12px] leading-[15px] ">{ride.state}</h1>
+              </div>
+            </div>
+          </div>
+          )) :
+            <>
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+            <Spinner />
+            </> 
+        }
+        
+      </div>
+    </React.Fragment>
   )
 }
 
-export default UpcomingRides
+export default UpcomingRides;;
